fd1a90f798df1ba990d4b7a7229fa6fb6a1707b5
Dave Stanton <davestanton.us@gmail.com>
2017-12-28 16:48:10 -0500

    Ecmascript leap.

---
+++ ecmascript/leap/leap.js
@@ -0,0 +1,16 @@
+class Leap {
+  isLeap(year) {
+    if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        if (year % 400 === 0) {
+          return true;
+        }
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
+}
+
+export default Leap;
