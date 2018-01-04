3a02c0859434798f601c481e09d814d3cdd3f273
Dave Stanton <davestanton.us@gmail.com>
2018-01-03 22:26:10 -0500

    Ecmascript RNA transcription.

---
+++ ecmascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,26 @@
+class Transcriptor {
+  toRna(dna) {
+    let rna = '';
+    for (const c of dna) {
+      switch (c) {
+        case 'A':
+          rna += 'U';
+          break;
+        case 'C':
+          rna += 'G';
+          break;
+        case 'G':
+          rna += 'C';
+          break;
+        case 'T':
+          rna += 'A';
+          break;
+        default:
+          throw 'Invalid input DNA.';
+      }
+    }
+    return rna;
+  }
+}
+
+export default Transcriptor;
