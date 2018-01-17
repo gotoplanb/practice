9da01bfeeacc1cb983cbc195fbe5c3a85f8101f3
Dave Stanton <davestanton.us@gmail.com>
2018-01-16 21:50:38 -0500

    Python update RNA transcription to use list comprehension.

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -3,8 +3,8 @@
 def to_rna(dna_strand):
     """Convert a DNA strand into an RNA strand"""
 
-    # Create an empty string to hold the conversted RNA string
-    rna_string = ''
+    # Create an empty array to hold the converted RNA string
+    rna_string = []
 
     # Use a dictionary to map DNA bases to RNA bases
     bases = {
@@ -19,8 +19,8 @@ def to_rna(dna_strand):
         # Check that the current base is a real DNA base
         if base in bases:
             # Get the associated RNA base and append to the RNA string
-            rna_string += bases[base]
+            rna_string.append(bases[base])
         else:
             raise ValueError('Invalid DNA base: ' + base)
 
-    return rna_string
+    return ''.join(rna_string)
