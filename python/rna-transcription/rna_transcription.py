7483df33c100d1a1c623e78bb6c144e4622cd0e7
Dave Stanton <davestanton.us@gmail.com>
2018-02-05 08:29:31 -0700

    Python RNA transcription.

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,18 +1,18 @@
 """RNA transcription"""
 
+# Use a dictionary to map DNA bases to RNA bases
+bases = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+}
+
 def to_rna(dna_strand):
     """Convert a DNA strand into an RNA strand"""
 
     # Create an empty array to hold the converted RNA string
     rna_string = []
-
-    # Use a dictionary to map DNA bases to RNA bases
-    bases = {
-        'G': 'C',
-        'C': 'G',
-        'T': 'A',
-        'A': 'U'
-    }
 
     # Iterate through each base in the DNA strand
     for base in dna_strand:
