ca527932b395025180aecfaf14f2e64fe9a8de09
Dave Stanton <davestanton.us@gmail.com>
2018-02-05 20:15:10 -0700

    Python word count.

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,20 +1,12 @@
-import string
+import re
 
 def word_count(phrase):
 
-    # Create an empty dictionary to hold our word counts
+    # Create an empty dictionary to hold our word counts.
     words = {}
 
-    # Remove non-desireable characters. This should be a regex.
-    letters_only = []
-    for letter in phrase:
-        if (letter in string.ascii_letters) or (letter == "'") or (letter in string.digits) :
-            letters_only.append(letter.lower())
-        else:
-            letters_only.append(" ")
-
-    # Split the phrase by a space
-    raw_words = ''.join(letters_only).split()
+    # Remove non-desireable characters and make letter lowercase.
+    raw_words = re.findall(r"[a-z\d]+(?:'[a-z])?", phrase.lower())
     
     for word in raw_words:
         if word in words:
